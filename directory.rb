ae366342d9d5e5a903dbc9b5a206a2831449de23
mikitaro-27 <matt.diak@gmail.com>
2021-12-01 12:16:37 +0000

    replace puts'ing students separetly with a loop

--- directory.rb
+++ directory.rb
@@ -16,17 +16,9 @@ students = [
 # print students' names
 puts "The students of Villains Academy"
 puts "---------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]  
-puts students[8]
-puts students[9]
-puts students[10]
+students.each { |student|
+  puts student
+}
 
 # prints the total number of students
 puts "Overall, we have #{students.count} great students"
