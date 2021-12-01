40c0eb15883f72bd1fe0110cbedec366fa97e7cc
mikitaro-27 <matt.diak@gmail.com>
2021-12-01 12:23:53 +0000

    extract the code into methods

--- directory.rb
+++ directory.rb
@@ -13,12 +13,24 @@ students = [
   "Norman Bates"
   ]
 
-# print students' names
-puts "The students of Villains Academy"
-puts "---------------"
-students.each { |student|
-  puts student
-}
+# Create a method for printing the header
+def print_header
+  puts "The students of Villains Academy"
+  puts "---------------"
+end
 
-# prints the total number of students
-puts "Overall, we have #{students.count} great students"
+# Create a method for printing students' names
+def print(names)
+  names.each { |student|
+    puts student
+  }
+end
+
+# Create a method for printing the footer
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
