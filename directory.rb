7d43c294112b32841329f1566ac05e56abdd466d
mikitaro-27 <matt.diak@gmail.com>
2021-12-01 13:48:40 +0000

    change student_profile in students array to be hash

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 # puts all students in an array
 students = [
-  ["Dr. Hannibal Lecter", :november],
-  ["Darth Vader", :november],
-  ["Nurse Ratched", :november],
-  ["Michael Corleone", :november],
-  ["Alex DeLarge", :november],
-  ["The Wicked Witch of the West", :november],
-  ["Terminator", :november],
-  ["Fredd Krueger", :november],
-  ["The Joker", :november],
-  ["Joffrey Baratheon", :november],
-  ["Norman Bates", :november]
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Fredd Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
   ]
 
 # Create a method for printing the header
@@ -21,8 +21,8 @@ end
 
 # Create a method for printing students' names
 def print(names)
-  names.each { |student|
-    puts student
+  names.each { |student_profile|
+    puts "#{student_profile[:name]} (#{student_profile[:cohort]} cohort)"
   }
 end
 
