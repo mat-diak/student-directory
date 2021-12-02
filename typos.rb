6523b13b0ed800c80b282eb422724b9ea5f980a5
mikitaro-27 <matt.diak@gmail.com>
2021-12-02 14:44:23 +0000

    add typos.rb case fwhen list is empty

--- typos.rb
+++ typos.rb
@@ -1,24 +1,24 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
+
   # create an empty array
   students = []
-  
+
   # get the first name
   name = gets.chomp
-  
+
   # while the name is not empty, repeat this code
   while !name.empty? do
-  
+
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    
+
     # get another name from the user
     name = gets.chomp
   end
-  
+
   # return the array of students
   students
 end
@@ -40,6 +40,10 @@ end
 
 
 students = input_students
-print_header
-print(students)
-print_footer(students)
+if students.length == 0
+  puts "There are no students in the list"
+else
+  print_header
+  print(students)
+  print_footer(students)
+end
