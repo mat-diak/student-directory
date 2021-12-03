9d0743828e52617333bcc49ca00c9f92e2f0e387
mikitaro-27 <matt.diak@gmail.com>
2021-12-03 12:05:40 +0000

    add add_student method

--- directory.rb
+++ directory.rb
@@ -1,16 +1,50 @@
 @students = []
 
+# prints options for interactive menu
+def print_menu
+  puts "--- 1. Input the students"
+  puts "--- 2. Show the students"
+  puts "--- 3. Save the students to students.csv"
+  puts "--- 4. Load students list from students.csv"
+  puts "--- 9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "!! Incorrect input. Type a number! !!"
+  end
+end
+
+def add_student(name, cohort = :november)
+  @students << {name: name, cohort: cohort.to_sym} # Create a student_profile hash with values
+end
+
 # asks user for students' names and creates a hash {name: ..., cohort: ...}
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-
-  # Get the first name
-  name = STDIN.gets.chomp
-
+  name = STDIN.gets.chomp # Get the first name
   while !name.empty? do
     # Add the name as part of hash to students
-    @students << {name: name, cohort: :november}
+    add_student(name)
     puts "Now we have #{@students.count} students"
     # Get a name new
     name = STDIN.gets.chomp
@@ -22,9 +56,15 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r") # Open the students.csv in read mode
   file.readlines.each { |csv_line| # Loop through lines of students.csv; split each line at ','
     name, cohort = csv_line.split(",")
-    @students << {name: name, cohort: cohort.to_sym} # Create a student_profile hash with values
+    add_student(name, cohort)
   }
   file.close # Close file
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
 end
 
 # prints the header
@@ -43,38 +83,6 @@ end
 # prints the footer
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
-end
-
-def show_students
-  print_header
-  print_students_list(@students)
-  print_footer(@students)
-end
-
-# prints options for interactive menu
-def print_menu
-  puts "--- 1. Input the students"
-  puts "--- 2. Show the students"
-  puts "--- 3. Save the students to students.csv"
-  puts "--- 4. Load students list from students.csv"
-  puts "--- 9. Exit"
-end
-
-def process(selection)
-  case selection
-  when "1"
-    @students = input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "!! Incorrect input. Type a number! !!"
-  end
 end
 
 def save_students
@@ -102,12 +110,6 @@ def try_load_students
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
-
+puts ARGV.join(",")
 try_load_students
 interactive_menu
